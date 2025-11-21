2f8189c332f690152a3e57676b432ac0c314a8fa
yu704176671 <904176617@qq.com>
2025-11-21 23:42:11 +0800

    Update supabase_config.py
    Signed-off-by: yu704176671 <904176617@qq.com>

--- supabase_config.py
+++ supabase_config.py
@@ -1,3 +1,3 @@
-# 错误的代码
+# 修正后的代码
 supabase_url = os.environ.get("SUPABASE_URL")
-supabase_key = os.environ.get("SUPABASE_KEY")  # 这行需要修改
+supabase_key = os.environ.get("SUPABASE_ANON_KEY")  # 修改后的代码
